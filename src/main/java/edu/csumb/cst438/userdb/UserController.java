c6ad7d88a1df6b72ec87deae28c816c7da2ff3ee
unknown <malasadi@csumb.edu>
2020-10-31 12:21:54 -0500

    dada343

--- src/main/java/edu/csumb/cst438/userdb/UserController.java
+++ src/main/java/edu/csumb/cst438/userdb/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.csumb.cst438.userdb.entities.User;
 
