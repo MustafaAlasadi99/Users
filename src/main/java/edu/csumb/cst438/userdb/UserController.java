7104525089d4e0e76a6adeab98668b5e1a50ba82
unknown <malasadi@csumb.edu>
2020-10-31 12:07:52 -0500

    dada

--- src/main/java/edu/csumb/cst438/userdb/UserController.java
+++ src/main/java/edu/csumb/cst438/userdb/UserController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.HttpStatus;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
 
 import edu.csumb.cst438.userdb.entities.User;
 
